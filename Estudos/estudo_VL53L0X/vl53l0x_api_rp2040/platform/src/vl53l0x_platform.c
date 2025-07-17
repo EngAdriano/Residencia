7a2bb4276dda3327eef262594105927da30c8f75
José Adriano <adriano.liukin@gmail.com>
2025-07-16 21:33:53 -0300

    Atualização

--- Estudos/estudo_VL53L0X/vl53l0x_api_rp2040/platform/src/vl53l0x_platform.c
+++ Estudos/estudo_VL53L0X/vl53l0x_api_rp2040/platform/src/vl53l0x_platform.c
@@ -263,6 +263,9 @@ VL53L0X_Error  VL53L0X_RdDWord(VL53L0X_DEV Dev, uint8_t index, uint32_t *data){
 #define VL53L0X_POLLINGDELAY_LOOPNB  250
 VL53L0X_Error VL53L0X_PollingDelay(VL53L0X_DEV Dev){
     VL53L0X_Error status = VL53L0X_ERROR_NONE;
+
+    busy_wait_ms(1); // 1 ms delay
+    /*
     LOG_FUNCTION_START("");
 
     const DWORD cTimeout_ms = 1;
@@ -273,5 +276,6 @@ VL53L0X_Error VL53L0X_PollingDelay(VL53L0X_DEV Dev){
     }
 
     LOG_FUNCTION_END(status);
+    */
     return status;
 }
