9d7335f7e464e11e6962868f9390ac7230481744
José Adriano <adriano.liukin@gmail.com>
2025-09-13 11:05:44 -0300

    Inclusão de bibliotecas

--- Projeto_Final_Amigo/VirtualGiro/VirtualGiro.c
+++ Projeto_Final_Amigo/VirtualGiro/VirtualGiro.c
@@ -7,6 +7,10 @@
 #include "semphr.h"
 #include "queue.h"
 
+#define WIFI_SSID "Your Wi-Fi SSID"
+#define WIFI_PASSWORD "Your Wi-Fi Password"
+
+
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
