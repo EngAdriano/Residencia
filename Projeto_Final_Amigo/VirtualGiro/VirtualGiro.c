e3fc955ea8835b95fe93d4df64794e1f3df9117e
José Adriano <adriano.liukin@gmail.com>
2025-09-15 08:24:25 -0300

    Atualização

--- Projeto_Final_Amigo/VirtualGiro/VirtualGiro.c
+++ Projeto_Final_Amigo/VirtualGiro/VirtualGiro.c
@@ -10,8 +10,8 @@
 #include "mpu6050_i2c.h"
 #include "ssd1306.h"
 
-#define WIFI_SSID     "Lu e Deza"
-#define WIFI_PASSWORD "liukin1208"
+#define WIFI_SSID     "xxxxxxxxx"
+#define WIFI_PASSWORD "xxxxxxxxx"
 
 #define SERVER_IP   "192.168.1.11"
 #define SERVER_PORT 12345
