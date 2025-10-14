9bbb3bbbb816d443917ae0d0bfbe55ad1132f552
José Adriano <adriano.liukin@gmail.com>
2025-10-14 08:24:07 -0300

    Atualização

--- Exercicios/U5C1Tarefas/pico_datalog/pico_datalog.c
+++ Exercicios/U5C1Tarefas/pico_datalog/pico_datalog.c
@@ -8,21 +8,10 @@
 
 #include "hw_config.h"
 
-// Definições da SPI
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-/*
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
-*/
-
-//#define WIFI_SSID "ITSelf"
-//#define WIFI_PASSWORD "code2020"
-#define WIFI_SSID "Lu e Deza"
-#define WIFI_PASSWORD "liukin1208"
+#define WIFI_SSID "ITSelf"
+#define WIFI_PASSWORD "code2020"
+//#define WIFI_SSID "Lu e Deza"
+//#define WIFI_PASSWORD "liukin1208"
 
 
 int main()
@@ -35,20 +24,6 @@ int main()
         printf("Wi-Fi init failed\n");
         return -1;
     }
-
-    /*
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
-    */
 
     // Enable wifi station
     cyw43_arch_enable_sta_mode();
